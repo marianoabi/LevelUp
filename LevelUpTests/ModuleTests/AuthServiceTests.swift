7310c43023e3f759f7d85b707ab9679eefe34802
Abigail Mariano <marianoabby@gmail.com>
2025-05-08 21:10:03 +0800

    Moved MockNetworkService class inside AuthServiceTests

--- LevelUpTests/ModuleTests/AuthServiceTests.swift
+++ LevelUpTests/ModuleTests/AuthServiceTests.swift
@@ -132,3 +132,28 @@ final class AuthServiceTests: XCTestCase {
     }
 }
 
+class MockNetworkService: NetworkServiceProtocol {
+    var mockResult: Result<AuthResponse, Error>?
+    
+    func request<T>(endpoint: LevelUp.Endpoint, completion: @escaping (Result<T, any Error>) -> Void) where T : Decodable {
+        guard let mockResult = mockResult else {
+            fatalError("Mock result not set")
+        }
+        
+        switch mockResult {
+        case .success(let response):
+            guard let responseT = response as? T else {
+                fatalError("Mock response mismatch")
+            }
+            completion(.success(responseT))
+        case .failure(let error):
+            completion(.failure(error))
+        }
+    }
+    
+    func upload(data: Data, endpoint: LevelUp.Endpoint, completion: @escaping (Result<Void, any Error>) -> Void) {
+        fatalError("Not implemented in mock")
+    }
+    
+}
+
